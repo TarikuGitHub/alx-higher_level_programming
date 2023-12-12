d94a920d1cd642bf5affea3019d78d01aa45ae5c
TarikuGitHub <tarikukussia2010@gmail.com>
2023-12-12 07:41:39 -0800

    wE

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,18 +1,12 @@
 #!/usr/bin/node
-// reads two text files and concatenates contents into new file
-const fs = require('fs');
-// fs is standard but needs import
 
-// each `readFile` and `writeFile` call is asynchronous
-fs.readFile(process.argv[2], (err, data) => {
-  if (err) throw err;
-  // data is bytestream before encoding
-  let output = data;
-  fs.readFile(process.argv[3], (err, data) => {
-    if (err) throw err;
-    output += data;
-    fs.writeFile(process.argv[4], output, (err) => {
-      if (err) throw err;
-    });
-  });
-});
+const fs = require('fs').promises;
+const { argv } = require('process');
+
+fs.readFile(argv[2], 'utf8')
+  .then(data => fs.writeFile(argv[4], data, 'utf8'))
+  .catch(err => console.error(err));
+
+fs.readFile(argv[3], 'utf8')
+  .then(data => fs.writeFile(argv[4], data, { flag: 'a' }, 'utf8'))
+  .catch(err => console.error(err));
