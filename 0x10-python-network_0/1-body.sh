ab315364eb26e482b6bcde4013338b197abfe7d9
TarikuGitHub <tarikukussia2010@gmail.com>
2024-01-09 04:37:02 -0800

    WE

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# shellcheck disable=SC2046
-if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
+#!/bin/bash                                                                   
+# a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response                                                  
+if [ "$(curl -sLI "$1" -X GET | grep "200 OK" | cut -d' ' -f2)" = '200' ]; then curl -sL "$1"; fi
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL and displays all HTTP methods the server will accept
-curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -f2- -d" "
+#  a Bash script that takes in a URL and displays all HTTP methods the server will accept
+curl -si "$1" | awk -F ": " '/Allow/ {print $2}'
