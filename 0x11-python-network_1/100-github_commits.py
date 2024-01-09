6329504f95b0fcb1b533c33ff6dba252b87627d8
TarikuGitHub <tarikukussia2010@gmail.com>
2024-01-09 05:15:32 -0800

    We

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""Python script that lists list 10 commits (from the most recent
-to oldest) of the repository “rails” by the user “rails”."""
+"""
+Python script that takes 2 arguments in order to solve this challenge.
 
-import requests
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
 from sys import argv
+import requests
+
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits?per_page=10"\
-          .format(argv[2], argv[1])
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    req = requests.get(url)
+    commits = req.json()
 
-    request = requests.get(url)
-    commits = request.json()
-    for commit in commits:
-        print("{}: {}".format(commit.get("sha"),
-                              commit.get("commit").get("author").get("name")))
+    try:
+        for index in range(10):
+            print("{}: {}".format(
+                commits[index].get("sha"),
+                commits[index].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
